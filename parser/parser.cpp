121da0a542455102c0458f08ae1b3cf5ffce6244
Shayan Ali Abbasi <shayanali422@gmail.com>
2019-08-16 01:56:14 +0500

    add data folder

--- parser/parser.cpp
+++ parser/parser.cpp
@@ -2,9 +2,8 @@
 #include <string>
 #include <fstream>
 #include <locale>
-#include <algorithm>
 
-// Struct to store self defining attributes
+// Struct to store self defining attributes (weight is binary encoding of word where mutaharik = '1' and sakin = '0'
 struct Lafz
 {
     std::wstring word;
@@ -33,15 +32,7 @@ std::wstring generate_weight(std::wstring murrab)
     }
     else if (murrab.size() == 1) // The word only had one character
     {
-        if (murrab[0] == L'آ') // Valid if that character is 'آ'
-        {
-            return L"10";
-        }
-        else // Otherwise, word cannot consist of only one character
-        {
-            std::cerr << "Cannot generate weight: The word is of character length 'one'";
-            exit(-2);
-        }
+        return (murrab.back() == L'آ' ? L"10" : L"1"); // Return 10 if it is آ otherwise 1
     }
 
     // We will get here if everything is valid
@@ -53,11 +44,11 @@ std::wstring generate_weight(std::wstring murrab)
     if (murrab[0] == L'آ')
         weight += L"10"; // If first letter is 'آ' then we prepend weight with "10"
     else
-        weight += L"1"; // Otherwise, "1" because first letter will always
+        weight += L"1"; // Otherwise, "1" because first letter will always be mutaharik
 
-    for (int i = 1; i < wordSize - 1; i++) // loop through all except first and last characters
+    for (int i = 1; i < wordSize - 1; i++) // loop through all except first and last characters in murrab
     {
-        wchar_t character = murrab[i];
+        wchar_t character = murrab[i]; // character currently under consideration
 
         if (character >= 1611 && character <= 1631)
             continue;
@@ -74,7 +65,6 @@ std::wstring generate_weight(std::wstring murrab)
 
         case L'/':
         {
-            // Todo()
             weight.back() = L'0';
             return weight;
         }
@@ -121,7 +111,7 @@ std::wstring generate_weight(std::wstring murrab)
             case 1628:
             case 1629:
             case 1630:
-            case 1631: // current letter has symbol other than sukoon (so it is mutaharik)
+            case 1631: // current letter has symbol other than sukoon,shad,khari zabr (so it is mutaharik)
             {
                 weight += L"1";
             }
@@ -159,94 +149,106 @@ std::wstring generate_weight(std::wstring murrab)
 
 int main()
 {
+    // Wide character streams to read words,murrab csv file and output to a words,murrab,weight txt file (direct conversion to csv gives garbage values)
     std::wifstream file_read;
     std::wofstream file_write;
 
-    SetConsoleOutputCP(CP_UTF8);
-
+    // Bind the Urdu UTF8 locale with file streams
     file_read.imbue(std::locale("ur_PK.UTF8"));
     file_write.imbue(std::locale("ur_PK.UTF8"));
 
-    file_read.open("C:/Users/Shayan Ali Abbasi/Desktop/File.txt");
-
+    // Open the file to convert and check for error
+    file_read.open("../data/words_murrab.cv");
     if (file_read.fail())
     {
+        std::cerr << "Unable to open specified file for reading.";
         exit(-1);
     }
 
-    file_write.open("C:/Users/Shayan Ali Abbasi/Desktop/words_weight.txt");
+    // Open the file to write and check for errors
+    file_write.open("../data/words_murrab_weight.txt");
+    if (file_write.fail())
+    {
+        std::cerr << "Unable to open specified file for writing.";
+    }
 
+    // This wchar variable will hold current character from the input stream as we go through all them in file
     wchar_t character = L'\0';
 
+    // Out lafz object that will store a triplet of word,murrab,weight
     Lafz lafz;
 
-    bool foundCommaDelim = false;
-    bool foundEndOfLine = false;
-    bool foundEndOfFile = false;
-    bool writeWord = false;
+    bool foundCommaDelim = false; // boolean to find out whether we have found a comma on current line (meaning end of word)
+    bool foundEndOfLine = false;  // boolean to find out whether we have found end of current line
+    bool foundEndOfFile = false;  // boolean to find out whether we have reached end of file while parsing
+    bool writeWord = false;       // boolean to find out whether we can write our lafz.word to file
 
-    file_read.get();
+    file_read.get(); // Ignore first garbage character in this file
 
+    // while we can get a character from read stream : insert it into  character variable (doing 2 things in one statement)
     while (character = file_read.get())
     {
-        switch (character)
+        switch (character) // Check the character we got from file
         {
-        case L',':
+        case L',': // We found the comma
         {
-            foundCommaDelim = true;
-            writeWord = true;
-            continue;
+            foundCommaDelim = true; // set flag to true
+            writeWord = true;       // now we can write word to other file (wofstream) | set the flag to true
+            continue;               // We don't want comma to get into lafz.word therefore move to next character
         }
         break;
 
-        case L'\n':
+        case L'\n': // We found end of line
         {
-            foundEndOfLine = true;
+            foundEndOfLine = true; // setting the flag to true
         }
         break;
         }
 
-        if (file_read.eof())
+        if (file_read.eof()) // will set flag to true as we have reached end of file | will be used later so last character is not used two times
         {
             foundEndOfFile = true;
         }
 
-        if (!foundCommaDelim)
+        if (!foundCommaDelim) // We have'nt found the comma in current line? Put the characters in lafz.word as we are currently reading characters of word
         {
             lafz.word += character;
         }
 
-        if (foundCommaDelim)
+        if (foundCommaDelim) // We have found comma in current line? Put the characters
         {
+            // Just making sure we add characters only if end of file is not reached or end of line: '\n' = end of line character will become part of murrab otherwise
             if (!foundEndOfLine && !foundEndOfFile)
                 lafz.murrab += character;
         }
 
-        if (writeWord)
+        if (writeWord) // We can write lafz.word?
         {
-            trim_whitespace(lafz.word);
+            trim_whitespace(lafz.word); // Remove leading and trailing spaces
 
-            file_write << lafz.word << L",";
+            file_write << lafz.word << L","; // Write lafz.word to file
 
-            writeWord = false;
+            writeWord = false; // setting to false as we cannot write the word again as it is lafz.murrab turn now
         }
 
-        if (foundEndOfLine || foundEndOfFile)
+        if (foundEndOfLine || foundEndOfFile) // We found end of line (exception for last line where there will be no end of line therefore end of file for that case)?
         {
-            trim_whitespace(lafz.murrab);
+            trim_whitespace(lafz.murrab); // Again we don't need spaces
 
-            file_write << lafz.murrab << L",";
+            file_write << lafz.murrab << L","; // Write murrab to file
 
-            lafz.weight = generate_weight(lafz.murrab);
+            lafz.weight = generate_weight(lafz.murrab); // Get the weight of murrab
 
-            file_write << lafz.weight << L"\n";
+            file_write << lafz.weight << L"\n"; // Write the weight to file
 
-            lafz = {};
+            lafz = {}; // Empty the lafz object
 
+            // Resetting everything as we will move to a new line
             foundCommaDelim = false;
             writeWord = false;
             foundEndOfLine = false;
 
+            // There is no new line but end of file? ( True For last line) Just break out of loop, we are finished
             if (foundEndOfFile)
                 break;
         }
