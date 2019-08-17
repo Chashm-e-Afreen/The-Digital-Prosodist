9b5811bc9202d35047ce4a708dde24d5b6ddc5a2
diesirae <belta.infinita@yandex.com>
2019-08-18 02:02:27 +0500

    Updates.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,4 +1,4 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 #include "ui_mainwindow.h"
 
 #include "meters-def.h"
@@ -7,79 +7,107 @@
 #include <QFile>
 #include <QTextStream>
 #include <QMessageBox>
-#include <QInputMethod>
-#include <QApplication>
-#include <QTextOption>
+
+#include <Windows.h>
+
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+  QMainWindow(parent),
+  ui(new Ui::MainWindow)
 {
-    QTextOption Q;
-    ui->setupUi(this);
-    QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+  ui->setupUi(this);
+
+  QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+  delete ui;
 }
 
 void MainWindow::on_pushButton_clicked()
 {
 
-    QString user_entered_combined_words = ui->plainTextEdit->toPlainText();
-    QStringList user_entered_individual_words = user_entered_combined_words.split(" ");
+  QString user_entered_combined_words = ui->plainTextEdit->toPlainText();
+  QStringList user_entered_individual_words = user_entered_combined_words.split(QRegExp("\\s+"), QString::SkipEmptyParts);
 
-    QString dict_file_path  = "C:/Users/Muhammad Rehan/Documents/GitHub/murgh-e-chaman/words_murrab_weight_unique.txt";
+  QString dict_file_path  = "C:/Users/Muhammad Rehan/Documents/GitHub/murgh-e-chaman/words_murrab_weight_unique.txt";
 
-    QFile file_read(dict_file_path);
-
-    if (!file_read.open(QIODevice::ReadOnly | QIODevice::Text))
+  QFile file_read(dict_file_path);
+  int count = 1;
+  if (!file_read.open(QIODevice::ReadOnly | QIODevice::Text))
     {
-        QMessageBox::warning(this, "Error", "Unable to open dictionary to fetch words");
-        return;
+      QMessageBox::warning(this, "Error", "Unable to open dictionary to fetch words");
+      return;
     }
 
-    QTextStream text_stream(&file_read);
-    int count =1;
-    foreach(QString  word, user_entered_individual_words)
+  QTextStream text_stream(&file_read);
+
+  int line_being_read = 0;
+
+  foreach(QString word, user_entered_individual_words)
     {
+      QChar first_letter = word.front();
 
-        text_stream.seek(0);
+      int line_to_be_read_from = 0;
 
-        while(!text_stream.atEnd())
+      auto LetterToLine_find_iterator =  LetterToLine_map.find(first_letter.unicode());
+
+      if (LetterToLine_find_iterator != LetterToLine_map.end())
         {
-            QString word_murrab_weight_combined;
+          line_to_be_read_from = LetterToLine_find_iterator->second;
+          ui->plainTextEdit->appendPlainText("");
+        }
+      else
+        {
+          ui->plainTextEdit->insertPlainText("Not_Found ");
+          continue;
+        }
 
-            text_stream >> word_murrab_weight_combined;
+      text_stream.seek(0);
 
-            QStringList word_murrab_weight_individual = word_murrab_weight_combined.split(",");
+      while(!text_stream.atEnd())
+        {
 
-            if (word_murrab_weight_individual[0] == word)
+          if (++line_being_read < line_to_be_read_from)
             {
-                QString weight = word_murrab_weight_individual[2];
-
-                auto arkan_find_iterator = Arkan_map.find(weight.toStdWString());
-
-                 if (arkan_find_iterator != Arkan_map.end())
-                 {
-                    std::wstring arkan_value_ws = arkan_find_iterator->second;
-                    QString arkan_value = QString::fromStdWString(arkan_value_ws);
-                     if (count==1)
-                    {
-                     ui->plainTextEdit->insertPlainText("\n");
-
-                    }
-                    ui->plainTextEdit->insertPlainText(arkan_value);
-                    ui->plainTextEdit->insertPlainText("  ");
-                 }
-                 else
-                 {
-                     ui->plainTextEdit->insertPlainText("No Rukan found for this word");
-                 }
-                  ++count;
-                 break;
+              text_stream.readLine();
+              continue;
             }
-         }
+
+          QString word_murrab_weight_combined;
+
+          text_stream >> word_murrab_weight_combined;
+
+          QStringList word_murrab_weight_individual = word_murrab_weight_combined.split(",");
+
+          if (word_murrab_weight_individual[0] == word)
+            {
+              QString weight = word_murrab_weight_individual[2];
+
+              auto arkan_find_iterator = Arkan_map.find(weight.toStdWString());
+
+                if (count==1)
+                {
+                     ui->plainTextEdit->insertPlainText("\n");
+                }
+              if (arkan_find_iterator != Arkan_map.end())
+                {
+                  std::wstring arkan_value_ws = arkan_find_iterator->second;
+                  QString arkan_value = QString::fromStdWString(arkan_value_ws);
+
+                  ui->plainTextEdit->insertPlainText(arkan_value + " ");
+                }
+
+              else
+                {
+                  ui->plainTextEdit->insertPlainText("No Rukan found");
+                }
+                ++count;
+              break;
+            }
+        }
     }
+
+  file_read.close();
+
 }
